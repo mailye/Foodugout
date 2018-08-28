99d2289ab32e72ca85c9437f4f0a3d7433e5365f
julie maieron <mailye@hotmail.com>
2018-08-28 12:25:30 +0200

    site

---
+++ jsjquery.js
@@ -0,0 +1,27 @@
+$(function(){
+	var ouvert = false;
+		$('#burger').on('click', function(){
+			
+			if(ouvert == false){
+			$('#navsecond').addClass('active');
+				ouvert=true;
+				//il faut mettre la variable ouvert en true pour dire que c'est ouvert
+			}else{
+			$('#navsecond').removeClass('active');
+			ouvert=false;
+			}
+
+		});
+
+		$('#entete').fullpage({
+		sectionSelector : '.section',
+		});
+
+	var bootstrapButton = $.fn.button.noConflict() 	
+	$.fn.bootstrapBtn = bootstrapButton 	
+
+		
+});
+	
+
+
